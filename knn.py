91386e55e3a29fe0b86acaf67496f0541f8566ea
Connor Olsen <olsenco@oregonstate.edu>
2023-10-18 02:45:24 -0700

    test with 1nn

--- knn.py
+++ knn.py
@@ -69,7 +69,7 @@ def main():
      #######################################
 
      results = []
-
+     ''''
      # Search over possible settings of k
      print("Performing 4-fold cross validation")
      for k in [1, 2, 5, 10, 100, 1000]:
@@ -108,10 +108,11 @@ def main():
 
      for k, train_acc, val_acc, val_acc_var in results:
           print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})".format(k, train_acc*100, val_acc*100, val_acc_var*100))
+     '''
      #######################################
 
 
-     """"
+     
      print("Performing 1-fold cross validation 1NN")
      for k in [1]:
           t0 = time.time()
@@ -140,7 +141,7 @@ def main():
 
      for k, train_acc, val_acc, val_acc_var in results:
           print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})".format(k, train_acc*100, val_acc*100, val_acc_var*100))
-     """
+     
 
 
 
