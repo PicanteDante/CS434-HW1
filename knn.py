e05ef507bafddc89900858d7db5f80d8bf3cd694
Connor Olsen <olsenco@oregonstate.edu>
2023-10-18 00:48:01 -0700

    try a bunch of ks

--- knn.py
+++ knn.py
@@ -72,6 +72,38 @@ def main():
 
      # Search over possible settings of k
      print("Performing 4-fold cross validation")
+     for k in [1, 2, 5, 10, 100, 1000]:
+          t0 = time.time()
+
+          #######################################
+          # TODO Compute train accuracy using whole set
+          #######################################
+          train_y_pred = []
+          for i in range(train_X.shape[0]):
+               query = train_X[i]
+               predicted_label = knn_classify_point(train_X, train_y, query, k)
+               train_y_pred.append(predicted_label)
+
+          train_correct = np.sum(train_y_pred == train_y)
+          train_acc = train_correct / len(train_y)
+
+          #######################################
+          # TODO Compute 4-fold cross validation accuracy
+          #######################################
+          val_acc, val_acc_var = cross_validation(train_X, train_y, num_folds=4, k=k)
+      
+          t1 = time.time()
+          print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})\t\t[exe_time = {:.2f}]".format(k, train_acc*100, val_acc*100, val_acc_var*100, t1-t0))
+
+          results.append((k, train_acc, val_acc, val_acc_var))
+
+     for k, train_acc, val_acc, val_acc_var in results:
+          print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})".format(k, train_acc*100, val_acc*100, val_acc_var*100))
+     #######################################
+
+
+     """"
+     print("Performing 1-fold cross validation 1NN")
      for k in [1]:
           t0 = time.time()
 
@@ -90,7 +122,7 @@ def main():
           #######################################
           # TODO Compute 4-fold cross validation accuracy
           #######################################
-          val_acc, val_acc_var = cross_validation(train_X, train_y, num_folds=len(train_X), k=k)
+          val_acc, val_acc_var = cross_validation(train_X, train_y, num_folds=4, k=k)
       
           t1 = time.time()
           print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})\t\t[exe_time = {:.2f}]".format(k, train_acc*100, val_acc*100, val_acc_var*100, t1-t0))
@@ -99,7 +131,12 @@ def main():
 
      for k, train_acc, val_acc, val_acc_var in results:
           print("k = {:5d} -- train acc = {:.2f}%  val acc = {:.2f}% ({:.4f})".format(k, train_acc*100, val_acc*100, val_acc_var*100))
-    #######################################
+     """
+
+
+
+
+
 
 
      #######################################
