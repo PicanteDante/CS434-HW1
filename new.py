af3f27ec487dd5eb0cadac1b692cfe887cfd8215
Dante <notapirate69yarhar@gmail.com>
2023-10-19 16:52:44 -0700

    Update new.py

--- new.py
+++ new.py
@@ -8,11 +8,17 @@ train_y = train_y[:,np.newaxis]
 
 data = traindata = np.genfromtxt('train.csv', delimiter=',', names=True, dtype=None)
 
-print(data[0])
+print((data[1]))
+print(type(data[0]))
+print(type(data[0]))
+print(type(data[0]))
+print(type(data[0]))
+print(type(data[0]))
+print(type(data[0]))
 print(data.dtype.names)
 
 
-incomelike = sum()
+#incomelike = sum()
 for i in range(0, len(data.dtype.names)):
 	column_sum = np.sum(data[:,i])
 	print("Sum of values in column ", column_index, " is: ", column_sum)
